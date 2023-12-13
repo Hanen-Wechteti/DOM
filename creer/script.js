5a1b19b798d259e2912b7ab33ec4634e918f013d
Hanen Wechteti <hanenwechteti27@gmail.com>
2023-12-13 09:16:27 +0100

    creer

---
+++ creer/script.js
@@ -0,0 +1,20 @@
+
+function creationDiv(){
+let article = document.querySelectorAll('article');
+for (let elem of article){
+let newsection = document.createElement('section');
+let newcolor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+let newparagraphe = document.createElement('p');
+newparagraphe.innerHTML = "blablablablablablablablablablablablablablbalbalblalblblbalbalbablabalbalbalbalablbalbla";
+newsection.appendChild(newparagraphe);
+newsection.style.backgroundColor = newcolor;
+elem.appendChild(newsection);
+
+}
+}
+let tab = ["alice", "julien", "alexis","cassidy", "winona","hanen"];
+for (let i = 0; i< tab.length; i++){
+creationDiv();
+}
+let section = document.querySelectorAll('section');
+for let
